592a3f333ca08f4eccc0c59876e0603b1532bb52
Joe Rocca <jprocca813@gmail.com>
2018-05-31 01:59:33 -0400

    Fixed issue that caused spaced paths to break for loop

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-sourcePath="$HOME/Documents/Notes"
+sourcePath="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Notes"
 destPath="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Notes"
 hashFilePath="$sourcePath/.note_hashes" #must delete .note_hashes if changing destination path or else files will not be added to new destination
 numOfJobs=0
 
 declare -A hashes
 if [ -f "$hashFilePath" ]; then
-	source $hashFilePath
+	source "$hashFilePath"
 fi
 
+#https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 for file in $sourcePath/*.md; do
 	newHash=$(shasum $file | awk '{print $1}')
 	oldHash=${hashes[$file]}
@@ -25,16 +28,19 @@ for file in $sourcePath/*.md; do
 		baseFileNameExt=$(basename $file)
 		baseFileName=${baseFileNameExt%%.*}
 		
-		cp "$file" "$destPath/$baseFileNameExt" 
+		if [ "$sourcePath" != "$destPath" ]; then
+			cp "$file" "$destPath/$baseFileNameExt" 
+		fi
 
 		pandoc $file -s -o "$destPath/$baseFileName.pdf" &
 
 		numOfJobs=$(($numOfJobs+1))	
 	fi
 done
+IFS=$SAVEIFS
 
 if (($numOfJobs > 0)); then		
-	declare -p hashes > $hashFilePath
+	declare -p hashes > "$hashFilePath"
 
 	echo "Converting $numOfJobs files..."
 	wait
